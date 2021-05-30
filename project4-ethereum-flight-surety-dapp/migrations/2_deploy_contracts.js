c4e10d801002de3264552b55d95e30743ad1683a
Andrey Stoev <andrey.stoev@mimacom.com>
2021-05-30 15:26:29 +0200

    - Fix migration script - give app contrat the address of data contract as a parameter

--- project4-ethereum-flight-surety-dapp/migrations/2_deploy_contracts.js
+++ project4-ethereum-flight-surety-dapp/migrations/2_deploy_contracts.js
@@ -5,9 +5,10 @@ const fs = require('fs');
 module.exports = function (deployer) {
 
     let firstAirline = '0xf17f52151EbEF6C7334FAD080c5704D77216b732';
+
     deployer.deploy(FlightSuretyData)
         .then(() => {
-            return deployer.deploy(FlightSuretyApp)
+            return deployer.deploy(FlightSuretyApp, FlightSuretyData.address)
                 .then(() => {
                     let config = {
                         localhost: {
