f4e4a206a296af2672f706a7a70f62a8b1f09a75
Andrey Stoev <andrey.stoev@mimacom.com>
2021-06-02 15:13:09 +0200

    - register 3 flights in contract deployment script and publish their data - make deploy contract code more readable

--- project4-ethereum-flight-surety-dapp/migrations/2_deploy_contracts.js
+++ project4-ethereum-flight-surety-dapp/migrations/2_deploy_contracts.js
@@ -3,31 +3,111 @@ const FlightSuretyData = artifacts.require("FlightSuretyData");
 const fs = require('fs');
 
 module.exports = async function (deployer) {
+
+    // GLOBAL VARIABLES
+
+    const firstAirlineAddress = '0xf17f52151EbEF6C7334FAD080c5704D77216b732';
+
+    const flight1 = {
+        airlineAddress: firstAirlineAddress,
+        flightNumber: "EC02689",
+        departureTime: Math.floor(new Date("02 Aug 2021 15:00:00 GMT") / 1000),
+        origin: "MXP",
+        destination: "IBZ"
+    };
+
+    const flight2 = {
+        airlineAddress: firstAirlineAddress,
+        flightNumber: "U202288",
+        departureTime: Math.floor(new Date("02 Aug 2021 16:15:00 GMT") / 1000),
+        origin: "MXP",
+        destination: "LTN"
+    };
+
+    const flight3 = {
+        airlineAddress: firstAirlineAddress,
+        flightNumber: "EC02835",
+        departureTime: Math.floor(new Date("02 Aug 2021 19:40:00 GMT") / 1000),
+        origin: "MXP",
+        destination: "BRI"
+    };
+
+    // CONTRACT DEPLOYMENT
+
     console.log("Start contract migration & setup ...")
 
-    console.log("- deploy contracts ...");
-    await deployer.deploy(FlightSuretyData);
-    await deployer.deploy(FlightSuretyApp, FlightSuretyData.address);
+    await deployContracts();
 
-    console.log("- obtaining instance to them ...");
+    console.log("- obtain instance to them ...");
     let dataContract = await FlightSuretyData.deployed();
     let appContract = await FlightSuretyApp.deployed();
 
-    console.log("- building trust between contracts ...");
-    await dataContract.authorizeContractCaller(FlightSuretyApp.address);
+    // CONTRACT INITIALIZATION
 
-    console.log("- register the first airline ...");
-    let firstAirlineAddress = '0xf17f52151EbEF6C7334FAD080c5704D77216b732';
-    await appContract.registerTheFirstAirline(firstAirlineAddress, "First Airline");
+    await buildTrustBetweenContracts();
 
-    console.log("- publish config json files ...");
-    let config = {
-        localhost: {
-            url: 'http://localhost:8545',
-            dataAddress: FlightSuretyData.address,
-            appAddress: FlightSuretyApp.address
-        }
+    await registerTheFirstAirline();
+    await fundTheFirstAirline();
+
+    await registerFlight(flight1);
+    await registerFlight(flight2);
+    await registerFlight(flight3);
+
+    // CONFIG DISTRIBUTION
+
+    publishConfigJsonFiles();
+
+    // ===
+    // Functions
+    // ===
+
+    async function deployContracts() {
+        console.log("- deploy contracts ...");
+        await deployer.deploy(FlightSuretyData);
+        await deployer.deploy(FlightSuretyApp, FlightSuretyData.address);
     }
-    fs.writeFileSync(__dirname + '/../src/dapp/config.json', JSON.stringify(config, null, '\t'), 'utf-8');
-    fs.writeFileSync(__dirname + '/../src/server/config.json', JSON.stringify(config, null, '\t'), 'utf-8');
+
+    async function buildTrustBetweenContracts() {
+        console.log("- build trust between contracts ...");
+        await dataContract.authorizeContractCaller(FlightSuretyApp.address);
+    }
+
+    async function registerTheFirstAirline() {
+        console.log("- register the first airline ...");
+        await appContract.registerTheFirstAirline(firstAirlineAddress, "easyJet");
+    }
+
+    async function fundTheFirstAirline() {
+        console.log("- fund the first airline ...");
+        let insurerFee = await dataContract.getInsurerFee.call();
+        await appContract.payAirlineInsurerFee({value: insurerFee, from: firstAirlineAddress});
+    }
+
+    async function registerFlight(flight) {
+        console.log(`- register flight ${flight.flightNumber} at ${flight.departureTime} from ${flight.origin} to ${flight.destination} ...`);
+        await appContract.registerFlight(
+            flight.flightNumber,
+            flight.departureTime,
+            flight.origin,
+            flight.destination,
+            {from: firstAirlineAddress}
+        );
+    }
+
+    function publishConfigJsonFiles() {
+        console.log("- publish config json files ...");
+        let config = {
+            localhost: {
+                url: 'http://localhost:8545',
+                dataAddress: FlightSuretyData.address,
+                appAddress: FlightSuretyApp.address,
+                firstAirlineAddress: firstAirlineAddress,
+                flight1: flight1,
+                flight2: flight2,
+                flight3: flight3
+            }
+        }
+        fs.writeFileSync(__dirname + '/../src/dapp/config.json', JSON.stringify(config, null, '\t'), 'utf-8');
+        fs.writeFileSync(__dirname + '/../src/server/config.json', JSON.stringify(config, null, '\t'), 'utf-8');
+    }
 }
