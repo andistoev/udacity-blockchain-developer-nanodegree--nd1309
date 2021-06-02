5e9154e23442f9fa893c61b8ef605a2ff77cb01a
Andrey Stoev <andrey.stoev@mimacom.com>
2021-06-02 09:08:34 +0200

    - optimize deploy contracts code

--- project4-ethereum-flight-surety-dapp/migrations/2_deploy_contracts.js
+++ project4-ethereum-flight-surety-dapp/migrations/2_deploy_contracts.js
@@ -4,21 +4,30 @@ const fs = require('fs');
 
 module.exports = function (deployer) {
 
-    let firstAirline = '0xf17f52151EbEF6C7334FAD080c5704D77216b732';
-
     deployer.deploy(FlightSuretyData)
         .then(() => {
             return deployer.deploy(FlightSuretyApp, FlightSuretyData.address)
-                .then(() => {
-                    let config = {
-                        localhost: {
-                            url: 'http://localhost:8545',
-                            dataAddress: FlightSuretyData.address,
-                            appAddress: FlightSuretyApp.address
-                        }
-                    }
-                    fs.writeFileSync(__dirname + '/../src/dapp/config.json', JSON.stringify(config, null, '\t'), 'utf-8');
-                    fs.writeFileSync(__dirname + '/../src/server/config.json', JSON.stringify(config, null, '\t'), 'utf-8');
-                });
+                .then(() => initializeContract());
         });
+
+    let firstAirlineAddress = '0xf17f52151EbEF6C7334FAD080c5704D77216b732';
+
+    function initializeContract() {
+        console.log("Initialize contract ...");
+        publishConfigJsonFiles();
+    }
+
+    function publishConfigJsonFiles() {
+        console.log("- publish config json files ...");
+        let config = {
+            localhost: {
+                url: 'http://localhost:8545',
+                dataAddress: FlightSuretyData.address,
+                appAddress: FlightSuretyApp.address
+            }
+        }
+        fs.writeFileSync(__dirname + '/../src/dapp/config.json', JSON.stringify(config, null, '\t'), 'utf-8');
+        fs.writeFileSync(__dirname + '/../src/server/config.json', JSON.stringify(config, null, '\t'), 'utf-8');
+    }
+
 }
