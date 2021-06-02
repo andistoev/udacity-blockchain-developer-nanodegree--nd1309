af2e583da3cf4c44deec49b3b18559e07451f8d5
Andrey Stoev <andrey.stoev@mimacom.com>
2021-06-02 23:44:08 +0200

    - cycle the oracles and prepare to submit the random status code

--- project4-ethereum-flight-surety-dapp/src/server/server.js
+++ project4-ethereum-flight-surety-dapp/src/server/server.js
@@ -8,6 +8,26 @@ import express from 'express';
 const FIRST_ORACLE_ACCOUNT_IDX = 20;
 const ORACLES_COUNT = 30;
 
+const FlightStatusCode = {
+    STATUS_CODE_UNKNOWN: 0,
+    STATUS_CODE_ON_TIME: 10,
+    STATUS_CODE_LATE_AIRLINE: 20,
+    STATUS_CODE_LATE_WEATHER: 30,
+    STATUS_CODE_LATE_TECHNICAL: 40,
+    STATUS_CODE_LATE_OTHER: 50
+};
+
+const flightStatusCodeArr = [
+    FlightStatusCode.STATUS_CODE_UNKNOWN,
+    FlightStatusCode.STATUS_CODE_ON_TIME,
+    FlightStatusCode.STATUS_CODE_LATE_AIRLINE,
+    FlightStatusCode.STATUS_CODE_LATE_WEATHER,
+    FlightStatusCode.STATUS_CODE_LATE_TECHNICAL,
+    FlightStatusCode.STATUS_CODE_LATE_OTHER
+];
+
+let hIndexesByOracleAddress = {};
+
 const config = Config['localhost'];
 const web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('http', 'ws')));
 const ONE_ETHER = web3.utils.toWei("1", "ether");
@@ -45,12 +65,36 @@ function registerOracle(idx, oracleAddress) {
 function fetchOracleIndexes(oracleAddress) {
     flightSuretyApp.methods.getMyIndexes().call({from: oracleAddress}, (error, result) => {
         if (error) throw error;
+        hIndexesByOracleAddress[oracleAddress] = result;
         console.log(`=> oracleAddress = ${oracleAddress}, result = ${result}`);
     });
 }
 
-function submitFlightStatusInfoFromMatchingOracles() {
-    console.log("Submitting flight status info from matching oracles");
+function submitFlightStatusInfoFromMatchingOracles(requestedIndex) {
+    console.log(`Submit flight status info from matching oracles to requestedIndex=${requestedIndex}`);
+
+    for (let i = 0; i < ORACLES_COUNT; i++) {
+        let oracleAddress = accounts[FIRST_ORACLE_ACCOUNT_IDX + i];
+        let indexes = hIndexesByOracleAddress[oracleAddress];
+        if (indexes.includes(requestedIndex)) {
+            submitFlightStatusInfo(oracleAddress);
+        }
+    }
+}
+
+function submitFlightStatusInfo(oracleAddress) {
+    let flightStatusCode = generateRandomFlightStatusCode();
+    console.log(`- submit flightStatusCode = <${flightStatusCode}> for oracleAddress = ${oracleAddress}`);
+
+}
+
+function generateRandomFlightStatusCode() {
+    let idx = getRandomInt(flightStatusCodeArr.length);
+    return flightStatusCodeArr[idx];
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
 
 async function registerEventListeners() {
@@ -72,7 +116,8 @@ function flightStatusInfoRequestedHandler(error, event) {
     let result = event.returnValues;
     let msg = `index: ${result.index}, airlineAddress: ${result.airlineAddress}, flightNumber: ${result.flightNumber}, departureTime: ${result.departureTime}`;
     showEvent("FlightStatusInfoRequested", msg);
-    submitFlightStatusInfoFromMatchingOracles();
+
+    submitFlightStatusInfoFromMatchingOracles(result.index);
 }
 
 function flightStatusInfoSubmittedHandler(error, event) {
